7f11249c598bf4c9b24fc315aa243cc153927042
JLH54 <julien_.com@hotmail.com>
2022-09-07 22:07:31 -0400

    Parallax effect done(infinie est PEUT ETRE a revoir)

--- Scripts/Parallax.cs
+++ Scripts/Parallax.cs
@@ -7,47 +7,82 @@ public class Parallax : MonoBehaviour
     //from brackeys :)
 
     //les choses a faire le parallax dessus
-    public Transform[] Backgrounds;
+    //public Transform[] Backgrounds;
 
     //leurs grosseurs
-    private float[] Scales;
+    //private float[] Scales;
 
     //comment on veut que sa sois beau(doit etre plus haut que 0)
-    public float Smoothing = 1f;
+    //public float Smoothing = 1f;
 
     //besoin de la position de la camera
-    [SerializeField]
-    private Transform MyCamera;
+    //[SerializeField]
+    //private Transform MyCamera;
 
-    private Vector3 PositionCameraPrecedente;
+    //private Vector3 PositionCameraPrecedente;
+
+    //private float PositionDeDepart;
+
+    //private float GrosseurEcran;
 
     // Start is called before the first frame update
+
+    private float lenght, startpos;
+
+    public GameObject cam;
+
+    public float parallaxEffect;
+
+
     void Start()
     {
-        PositionCameraPrecedente = MyCamera.position;
+        //PositionCameraPrecedente = MyCamera.position;
+        //PositionDeDepart = MyCamera.position.x;
+        //GrosseurEcran = GetComponent<SpriteRenderer>().bounds.size.x;
+        //Scales = new float[Backgrounds.Length];
 
-        Scales = new float[Backgrounds.Length];
+        //for (int i=0; i<Backgrounds.Length; i++)
+        //{
+        //Scales[i] = Backgrounds[i].position.z * -1;
+        //}
 
-        for (int i=0; i<Backgrounds.Length; i++)
-        {
-            Scales[i] = Backgrounds[i].position.z * -1;
-        }
+        startpos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < Backgrounds.Length; i++)
-        {
-            float parallax = (PositionCameraPrecedente.x - MyCamera.position.x) * Scales[i];
+        //for (int i = 0; i < Backgrounds.Length; i++)
+        //{
+        //    float parallax = (PositionCameraPrecedente.x - MyCamera.position.x) * Scales[i];
 
-            float BackgroundTargetPosX = Backgrounds[i].position.x + parallax;
+        //    float BackgroundTargetPosX = Backgrounds[i].position.x + parallax;
 
-            Vector3 BackgroundTargetPos = new Vector3(BackgroundTargetPosX, Backgrounds[i].position.y, Backgrounds[i].position.z);
+        //    Vector3 BackgroundTargetPos = new Vector3(BackgroundTargetPosX, Backgrounds[i].position.y, Backgrounds[i].position.z);
 
-            Backgrounds[i].position = Vector3.Lerp(Backgrounds[i].position, BackgroundTargetPos, Smoothing * Time.deltaTime);
-        }
+        //    Backgrounds[i].position = Vector3.Lerp(Backgrounds[i].position, BackgroundTargetPos, Smoothing * Time.deltaTime);
+        //}
 
-        PositionCameraPrecedente = MyCamera.position;
+        //PositionCameraPrecedente = MyCamera.position;
+
+        //float temp = (MyCamera.transform.position.x * (1 - Smoothing));
+
+        //if(temp> PositionDeDepart + GrosseurEcran)
+        //{
+        //    PositionDeDepart += GrosseurEcran;
+        //}
+        //else if(temp< PositionDeDepart - GrosseurEcran)
+        //{
+        //    PositionDeDepart -= GrosseurEcran;
+        //}
+
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + lenght) startpos += lenght;
+        else if (temp < startpos - lenght) startpos -= lenght;
     }
 }
