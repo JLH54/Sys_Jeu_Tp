e82893f287595456d5e46a73d5551fe40db4adf6
JLH54 <julien_.com@hotmail.com>
2022-10-04 03:28:15 -0400

    added star requirements [04-10-2022]

--- Scripts/Levels/EndLevels.cs
+++ Scripts/Levels/EndLevels.cs
@@ -5,11 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class EndLevels : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject b_ReturnToLvlSelector;
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Finish"))
         {
-            SceneManager.LoadScene("LvlSelector");
+            b_ReturnToLvlSelector.SetActive(true);
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            Destroy(gameObject.GetComponent<PlayerMovementInLevels>());
+
         }
     }
 }
